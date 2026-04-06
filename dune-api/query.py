9fa3d64757e232aa32fc3f7ecc9a450ffd6113e6
jgeofil <jeremy.geo@gmail.com>
2026-04-06 15:24:59 -0400

    chore: downgrade python requirement to 3.14 and update lockfile dependencies

--- dune-api/query.py
+++ dune-api/query.py
@@ -1,4 +1,5 @@
 from dune_client.client import DuneClient
 
-dune = DuneClient("$DUNE_API_KEY")
+dune = DuneClient("1rPJE7MnWH9TQq14AC2UhSWLyaeXfHpv")
 query_result = dune.get_latest_result(4124453)
+print(query_result)
